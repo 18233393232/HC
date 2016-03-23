56b7981a09cc831b8a23771a62d10d86bea6f965
刘海潮 A <18233393232@qq.com>
2016-03-23 17:57:34 +0800

    创建了项目

---
+++ GitHub/GitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub
+//
+//  Created by 刘海潮 on 16/3/23.
+//  Copyright © 2016年 刘海潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHub/GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub
+//
+//  Created by 刘海潮 on 16/3/23.
+//  Copyright © 2016年 刘海潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
